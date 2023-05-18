6c5c1bec07ecffb7bce73dc6137969bad4a6e120
juryokun <capeta.kozo@gmail.com>
2023-05-18 23:33:47 +0900

    add template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::{get, launch, routes};
+use rocket_dyn_templates::{Template, handlebars, context};
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/templating/<arg_foo>")]
+pub fn templating(arg_foo: &str) -> Template {
+    Template::render("index", context! {
+        foo: arg_foo,
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/login", routes![templating])
+        .attach(Template::fairing())
 }
+
