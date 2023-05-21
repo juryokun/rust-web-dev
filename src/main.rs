6f8cb0cd88084d0f83587f2ff541bcb1ec12dc00
juryokun <capeta.kozo@gmail.com>
2023-05-21 23:20:19 +0900

    add login form

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rocket;
 
 use rocket::{get, launch, routes};
 use rocket_dyn_templates::{Template, handlebars, context};
+use rocket::serde::Deserialize;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -10,17 +11,52 @@ fn index() -> &'static str {
 }
 
 #[get("/templating/<arg_foo>")]
-pub fn templating(arg_foo: &str) -> Template {
+pub fn login(arg_foo: &str) -> Template {
     Template::render("index", context! {
         foo: arg_foo,
     })
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![index])
-        .mount("/login", routes![templating])
-        .attach(Template::fairing())
+#[get("/reception")]
+pub fn reception() -> Template {
+    Template::render("reception", context! {foo: ""})
 }
 
+#[launch]
+fn rocket() -> _ {
+    let rocket = rocket::build()
+        .mount("/", routes![index, reception])
+        .attach(Template::fairing());
+
+    let figment = rocket.figment();
+    #[derive(Deserialize, Debug)]
+    #[serde(crate = "rocket::serde")]
+    struct Config {
+        port: u16,
+        foo: String
+    }
+
+    let config: Config = figment.extract().expect("config");
+    println!("{:?}", config);
+
+    rocket
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::uri;
+    use rocket::http::{ContentType, Status};
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn index() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let mut response = client.get(uri!(super::index)).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::Plain));
+        assert!(response.headers().get_one("X-Content-Type-Options").is_some());
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+}
