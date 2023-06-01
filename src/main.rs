9a0fc9217e3c91e4026be73fbba72165cdec1665
juryokun <capeta.kozo@gmail.com>
2023-06-01 23:17:56 +0900

    post request

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ extern crate rocket;
 use rocket::{get, launch, routes};
 use rocket_dyn_templates::{Template, handlebars, context};
 use rocket::serde::Deserialize;
+use rocket::form::Form;
+use rocket::http::RawStr;
+
+#[derive(FromForm)]
+pub struct LoginInfo<'r> {
+    user_cd: &'r str,
+    password: &'r str,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/templating/<arg_foo>")]
-pub fn login(arg_foo: &str) -> Template {
-    Template::render("index", context! {
-        foo: arg_foo,
-    })
+#[post("/login", data = "<login_info>")]
+pub fn login(login_info: Form<LoginInfo<'_>>) -> Template {
+    println!("{}", login_info.user_cd);
+    println!("{}", login_info.password);
+    Template::render("reception", context! {foo: ""})
 }
 
 #[get("/reception")]
@@ -25,7 +33,7 @@ pub fn reception() -> Template {
 #[launch]
 fn rocket() -> _ {
     let rocket = rocket::build()
-        .mount("/", routes![index, reception])
+        .mount("/", routes![index, reception, login])
         .attach(Template::fairing());
 
     let figment = rocket.figment();
@@ -53,6 +61,20 @@ mod test {
     fn index() {
         let client = Client::tracked(rocket()).expect("valid rocket instance");
         let mut response = client.get(uri!(super::index)).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::Plain));
+        assert!(response.headers().get_one("X-Content-Type-Options").is_some());
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
+
+    #[test]
+    fn login() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let mut response = client.post(uri!(super::login))
+            .header(ContentType::Form)
+            .body("user_cd=aaa&password=bbb")
+            .dispatch();
 
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.content_type(), Some(ContentType::Plain));
